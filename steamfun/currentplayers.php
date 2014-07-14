9bc94863dd42d3c55197ed184dad092a3a270767
David Petri <petridw@gmail.com>
2014-07-14 00:23:50 -0400

    displays current dota 2 players

--- steamfun/currentplayers.php
+++ steamfun/currentplayers.php
@@ -1,10 +1,19 @@
 <html>
 <head>
-<title>Concurrent Players</title>
+<title>Current Dota 2 Players</title>
 </head>
 <body>
-<?php echo '<p>Hello!</p>';
-echo '<p>Hello again!</p>;
+<?php
+$json = file_get_contents("http://api.steampowered.com/ISteamUserStats/GetNumberOfCurrentPlayers/v1?key=D4667C8EFE74F4FA70DC5EC2CBDE64D8&appid=570");
+
+$jsonIterator = new RecursiveIteratorIterator(
+	new RecursiveArrayIterator(json_decode($json, TRUE)),
+	RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+	if($key == "player_count"){echo $val;}
+}
+
 ?>
 </body>
 </html>
